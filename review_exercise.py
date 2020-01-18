4a3b62e49d0d77c1ee277a2f00d2573af1140a5c
Amos Tang <amos1024@gmail.com>
2020-01-18 14:31:25 +0800

    add filtering function

--- review_exercise.py
+++ review_exercise.py
@@ -4,7 +4,7 @@ with open('reviews.txt','r') as f:
     for line in f:
         data.append(line)
         count += 1 
-        if count % 1000 == 0:
+        if count % 10000 == 0:
             print(len(data))
 print('檔案讀取完了, 共有', len(data), '筆資料')
 
@@ -14,3 +14,9 @@ for d in data:
 
 average = sum_len/len(data)
 print('留言的平均長度為', average)
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new),'筆留言長度小於100')
